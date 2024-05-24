340be4f5956324d4bc21a8fa23839f4ebe609c4f
kosekeri <151658454+kosekeri@users.noreply.github.com>
2024-05-24 12:48:04 +0100

    Add files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+window.alert("Hello, World!");
+
+document.write("Eryka Kosek, First JavaScript Assignment");
+
+var A ="Eryka and Bartek"
+var B ="Builing home"
+document.write(A+B);
+
+document.write(3+3);
